9eee7ec84eca784e2046556b9327e8c4b92c58e3
jose38297 <gayaguedjali@yahoo.fr>
2026-03-10 11:49:15 +0100

    fix: correction ChromeDriver sur Windows GitHub Actions

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,3 +1,4 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
@@ -18,13 +19,23 @@ class DriverFactory:
                 options.add_argument("--headless=new")
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--disable-gpu")
             options.add_argument("--window-size=1920,1080")
             options.add_argument("--disable-notifications")
+            options.add_argument("--remote-debugging-port=9222")
             options.add_experimental_option("excludeSwitches", ["enable-logging"])
-            driver = webdriver.Chrome(
-                service=ChromeService(ChromeDriverManager().install()),
-                options=options
-            )
+
+            chrome_path = os.environ.get("CHROME_PATH", "")
+            if chrome_path:
+                options.binary_location = chrome_path
+
+            try:
+                driver = webdriver.Chrome(
+                    service=ChromeService(ChromeDriverManager().install()),
+                    options=options
+                )
+            except Exception:
+                driver = webdriver.Chrome(options=options)
 
         elif browser.lower() == "firefox":
             options = webdriver.FirefoxOptions()
