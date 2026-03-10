1e5844a36c889fcc7e2c42e3bce81d78fcb880fa
jose38297 <gayaguedjali@yahoo.fr>
2026-03-10 14:30:03 +0100

    fix: ajout chromedriver-binary-auto et encodage UTF-8

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -11,6 +11,8 @@ class DriverFactory:
         headless = headless if headless is not None else Config.HEADLESS
 
         if browser.lower() == "chrome":
+            import chromedriver_binary
+
             options = webdriver.ChromeOptions()
             if headless:
                 options.add_argument("--headless=new")
@@ -20,19 +22,11 @@ class DriverFactory:
             options.add_argument("--window-size=1920,1080")
             options.add_argument("--disable-notifications")
             options.add_argument("--disable-extensions")
-            options.add_argument("--remote-debugging-port=9222")
             options.add_experimental_option(
                 "excludeSwitches", ["enable-logging"]
             )
 
-            chromedriver_path = os.environ.get("CHROMEWEBDRIVER", "")
-            if chromedriver_path:
-                driver_exe = os.path.join(chromedriver_path, "chromedriver.exe")
-                service = ChromeService(executable_path=driver_exe)
-            else:
-                service = ChromeService()
-
-            driver = webdriver.Chrome(service=service, options=options)
+            driver = webdriver.Chrome(options=options)
 
         else:
             raise ValueError(f"Navigateur non supporte: {browser}")
